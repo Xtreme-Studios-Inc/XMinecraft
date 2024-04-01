105075539947d45aaa890706e00c5c4c0e97c97e
XsAndre-L <ahloubser12@gmail.com>
2024-04-01 16:43:30 +0200

    first commit

---
+++ src/main/java/com/xminecraft/XMinecraftDataGenerator.java
@@ -0,0 +1,11 @@
+package com.xminecraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class XMinecraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
